a2a4449ec9dc2188999be340e6572108c7ff44d5
Aashish567 <59502875+Aashish567@users.noreply.github.com>
2023-11-17 17:55:22 +0530

    details.js

--- src/components/details.js
+++ src/components/details.js
@@ -10,17 +10,20 @@ function Details() {
     const details = data.find((item)=>item.id==id);
 
   return (
-    <div style={{color:"black", textAlign:"center"}}>
+    
+    <div style={{color:"black", textAlign:"center", height:"100vh", backgroundColor:"black"}}>
 
-        <h1>Details Page With Id: <span style={{color:"red"}}>{id}</span></h1>
-        <img style={{marginTop:"20px"}} src="https://picsum.photos/200?random=${post.id" alt={details.title} />
-        <h3>User Id: <span style={{color:"red"}}>{details.userId}</span></h3>
-        <h3>Title: <span style={{color:"red"}}>{details.title}</span></h3>
-        <h3>Body: <span style={{color:"red"}}>{details.body}</span></h3><br></br>
+        <h1><span style={{color:"yellow"}}>Details Page With Id:</span> <span style={{color:"white"}}>{id}</span></h1>
+        <img style={{marginTop:"20px", borderRadius:"10px"}} src="https://picsum.photos/200?random=${post.id" alt={details.title} />
+        <h3><span style={{color:"yellow"}}>User Id:</span> <span style={{color:"white"}}>{details.userId}</span></h3>
+        <h3><span style={{color:"yellow"}}>Title:</span> <span style={{color:"white"}}>{details.title}</span></h3>
+        <h3><span style={{color:"yellow"}}>Body:</span> <span style={{color:"white"}}>{details.body}</span></h3><br></br>
         <Link to="/">
-        <button style={{color:"#ffffff", backgroundColor:"black", fontWeight:"bold"}} className='btn btn'>Go Back</button>
+        <button style={{color:"#ffffff", backgroundColor:"red", fontWeight:"bold"}} className='btn btn'>Go Back</button>
         </Link>
     </div>
+    
+    
   )
 }
 
